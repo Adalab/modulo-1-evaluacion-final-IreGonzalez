a101c1f66232dbdab4859c3858b3dcfd11b1c8f5
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-11-10 12:40:50 +0100

    add digital background

--- src/js/01navigation.js
+++ src/js/01navigation.js
@@ -1,6 +1,8 @@
-function handleMenu() {
+function handleMenu(ev) {
+    ev.preventDefault();
     navList.classList.toggle('hidden')
     nav.classList.toggle('hidden')
+    nav.classList.toggle('header__navigation--link')
 }
 
 nav.addEventListener('click', handleMenu);
