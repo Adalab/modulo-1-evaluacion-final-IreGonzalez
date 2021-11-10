1c5c30e39b33b8ec9aa7a86d178b482648f3ad38
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-11-10 22:12:49 +0100

    doing textcounter for formtext

--- src/js/00variables.js
+++ src/js/00variables.js
@@ -1,4 +1,7 @@
 'use strict'
+
+//VARIABLES
 const nav = document.querySelector('.js_nav');
 const navList = document.querySelector('.js_navList');
 const navClose = document.querySelector('.js_navClose');
+const text = document.querySelector('js_formText');
---
+++ src/js/02form.js
@@ -0,0 +1,9 @@
+function textCounter(ev) {
+    let text = text.value;
+    for (let i = 1; i < text.length; i++) {
+        if (text.length === 40) {
+            //debe añadir un salto de línea al texto con \n
+        }
+    }
+
+}
